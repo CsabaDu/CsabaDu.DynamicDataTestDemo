d565cc8d107c8832cdbbcd4bff6b914ae39990d5
CsabaDu <csabadudas@t-email.hu>
2024-05-22 16:57:24 +0200

    MyTypeTests members restructured

--- MyTypeTests.cs
+++ MyTypeTests.cs
@@ -3,24 +3,13 @@ namespace CsabaDu.DynamicDataTestDemo;
 [TestClass]
 public sealed class MyTypeTests
 {
-    #region Test prep
-    #region Fields
-    #region Static fields
+    #region General test preparation
     private const string TestLabel = nameof(TestLabel);
     private const string DifferentLabel = nameof(DifferentLabel);
-    private static readonly MyTypeTests Instance = new();
-    private const string DisplayName = nameof(GetDisplayName);
-    #endregion Static fields
 
     private int _quantity;
     private string _label;
     private MyType _myType;
-    #endregion Fields
-
-    #region Static properties
-    private static IEnumerable<object[]> EqualsMyTypeMyTypeArgs => Instance.GetEqualsMyTypeMyTypeArgs();
-    private static IEnumerable<object[]> EqualsObjectArgs => Instance.GetEqualsObjectArgs();
-    #endregion Static properties
 
     [TestInitialize]
     public void InitMyTypeTests()
@@ -29,76 +18,55 @@ public sealed class MyTypeTests
         _label = TestLabel;
         _myType = GetMyType();
     }
-    #endregion Test prep
 
-    #region Test methods
-    #region bool Equals
-    #region MyType.Equals(object)
-    [TestMethod]
-    [DynamicData(nameof(EqualsObjectArgs), DynamicDataSourceType.Property, DynamicDataDisplayName = DisplayName)]
-    public void Equals_arg_object_returns_expected(string testCase, bool expected, MyType myType, object obj)
+    private static int GetRandomQuantity(int? excluded = null)
     {
-        // Arrange
-        // Act
-        var actual = myType.Equals(obj);
+        Random random = new();
+        int quantity = getRandomQuantity();
 
-        // Assert
-        Assert.AreEqual(expected, actual);
+        if (excluded is null)
+        {
+            return quantity;
+        }
+
+        while (quantity == excluded.Value)
+        {
+            quantity = getRandomQuantity();
+        }
+
+        return quantity;
+
+        #region Local methods
+        int getRandomQuantity()
+        {
+            return random.Next(int.MinValue, int.MaxValue);
+        }
+        #endregion
     }
-    #endregion MyType.Equals(object)
 
-    #region IEqualityComparer.Equals(MyType, MyType)
-    [TestMethod]
-    [DynamicData(nameof(EqualsMyTypeMyTypeArgs), DynamicDataSourceType.Property, DynamicDataDisplayName = DisplayName)]
-    public void Equals_args_MyType_MyType_returns_expected(string testCase, bool expected, MyType x, MyType y)
+    private MyType GetMyType()
     {
-        // Arrange
-        // Act
-        var actual = _myType.Equals(x, y);
-
-        // Assert
-        Assert.AreEqual(expected, actual);
+        return new(_quantity, _label);
     }
-    #endregion IEqualityComparer.Equals(MyType, MyType)
-    #endregion bool Equals
+    #endregion
 
-    #region int GetHashCode
-    #region MyType.GetHashCode()
-    [TestMethod]
-    public void GetHashCode_returns_expected()
+    #region Members for dynamic data tests
+    private const string DisplayName = nameof(GetDisplayName);
+    private static readonly MyTypeTests Instance = new();
+
+    private static IEnumerable<object[]> EqualsMyTypeMyTypeArgs => Instance.GetEqualsMyTypeMyTypeArgs();
+    private static IEnumerable<object[]> EqualsObjectArgs => Instance.GetEqualsObjectArgs();
+
+    public static string GetDisplayName(MethodInfo methodInfo, object[] args)
     {
-        // Arrange
-        int expected = HashCode.Combine(_quantity, _label);
+        string methodName = methodInfo.Name;
+        string testCase = (string)args[0];
 
-        // Act
-        var actual = _myType.GetHashCode();
-
-        // Assert
-        Assert.AreEqual(expected, actual);
+        return $"{methodName}: {testCase}";
     }
-    #endregion MyType.GetHashCode()
+    #endregion
 
-    #region IEqualityComparer.GetHashCode(MyType)
-    [TestMethod]
-    public void GetHashCode_arg_MyType_returns_expected()
-    {
-        // Arrange
-        _quantity = GetRandomQuantity();
-        MyType other = GetMyType();
-        int expected = _quantity.GetHashCode();
-
-        // Act
-        var actual = _myType.GetHashCode(other);
-
-        // Assert
-        Assert.AreEqual(expected, actual);
-    }
-    #endregion IEqualityComparer.GetHashCode(MyType)
-    #endregion int GetHashCode
-    #endregion Test methods
-
-    #region Test helpers
-    #region Dynamic data
+    #region Dynamic data sources
     private IEnumerable<object[]> GetEqualsObjectArgs()
     {
         _myType = GetMyType();
@@ -120,7 +88,7 @@ public sealed class MyTypeTests
         testCase = "Different Quantity, same Label => false";
         _quantity = GetRandomQuantity(_quantity);
         obj = GetMyType();
-        expected= false;
+        expected = false;
         yield return argsToObjectArray();
 
         testCase = "Different Quantity, different Label => false";
@@ -191,44 +159,71 @@ public sealed class MyTypeTests
         }
         #endregion
     }
-    #endregion Dynamic data
-
-    private static int GetRandomQuantity(int? excluded = null)
-    {
-        Random random = new();
-        int quantity = getRandomQuantity();
-
-        if (excluded is null)
-        {
-            return quantity;
-        }
-
-        while (quantity == excluded.Value)
-        {
-            quantity = getRandomQuantity();
-        }
-
-        return quantity;
-
-        #region Local methods
-        int getRandomQuantity()
-        {
-            return random.Next(int.MinValue, int.MaxValue);
-        }
-        #endregion
-    }
-
-    private MyType GetMyType()
-    {
-        return new(_quantity, _label);
-    }
-
-    public static string GetDisplayName(MethodInfo methodInfo, object[] args)
-    {
-        string methodName = methodInfo.Name;
-        string testCase = (string)args[0];
-
-        return $"{methodName}: {testCase}";
-    }
     #endregion
+
+    #region Test methods
+    #region bool Equals
+    #region MyType.Equals(object)
+    [TestMethod]
+    [DynamicData(nameof(EqualsObjectArgs), DynamicDataSourceType.Property, DynamicDataDisplayName = DisplayName)]
+    public void Equals_arg_object_returns_expected(string testCase, bool expected, MyType myType, object obj)
+    {
+        // Arrange
+        // Act
+        var actual = myType.Equals(obj);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+    #endregion MyType.Equals(object)
+
+    #region IEqualityComparer.Equals(MyType, MyType)
+    [TestMethod]
+    [DynamicData(nameof(EqualsMyTypeMyTypeArgs), DynamicDataSourceType.Property, DynamicDataDisplayName = DisplayName)]
+    public void Equals_args_MyType_MyType_returns_expected(string testCase, bool expected, MyType x, MyType y)
+    {
+        // Arrange
+        // Act
+        var actual = _myType.Equals(x, y);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+    #endregion IEqualityComparer.Equals(MyType, MyType)
+    #endregion bool Equals
+
+    #region int GetHashCode
+    #region MyType.GetHashCode()
+    [TestMethod]
+    public void GetHashCode_returns_expected()
+    {
+        // Arrange
+        int expected = HashCode.Combine(_quantity, _label);
+
+        // Act
+        var actual = _myType.GetHashCode();
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+    #endregion MyType.GetHashCode()
+
+    #region IEqualityComparer.GetHashCode(MyType)
+    [TestMethod]
+    public void GetHashCode_arg_MyType_returns_expected()
+    {
+        // Arrange
+        _quantity = GetRandomQuantity();
+        MyType other = GetMyType();
+        int expected = _quantity.GetHashCode();
+
+        // Act
+        var actual = _myType.GetHashCode(other);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+    #endregion IEqualityComparer.GetHashCode(MyType)
+    #endregion int GetHashCode
+    #endregion Test methods
 }
